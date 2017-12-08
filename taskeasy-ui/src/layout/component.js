a283b79f87feb8fd25f0a81b734c913cd33afede
Clint Lehmberg <clintlehmberg@gmail.com>
2017-12-07 18:58:42 -0700

    update css file

--- taskeasy-ui/src/layout/component.js
+++ taskeasy-ui/src/layout/component.js
@@ -1,7 +1,8 @@
 import React, {Component} from 'react'
 import 'bootstrap/dist/css/bootstrap.css'
-import { Table } from 'reactstrap'
 import axios from 'axios'
+import { Table } from 'reactstrap'
+
 import './container.css'
 
 export class Layout extends Component {
